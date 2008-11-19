dafcc01bc71f9cd79b842477cc3a3be4271e00a6
Kamil Kisiel <kamil@zymeworks.com>
2008-11-19 18:42:34 +0000

    Made distutils-installable package

---
+++ passpersist/__init__.py
@@ -0,0 +1,1 @@
+from passpersist import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PassPersist',
+        version='0.1',
+        description='Net-SNMP pass_persist plugin base classes',
+        author='Kamil Kisiel',
+        author_email='kamil@kamilkisiel.net',
+        url='http://trac.lan.zymeworks.com/',
+        packages=['passpersist'],
+        )
